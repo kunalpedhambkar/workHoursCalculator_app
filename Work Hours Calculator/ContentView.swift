a43b9f53b83b27aa7042d76b2df320c40a9b4382
kunalrajeshpedhambkar <pedhambkarkunal@gmail.com>
2025-11-07 19:26:42 +1100

    Initial Commit

---
+++ Work Hours Calculator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Work Hours Calculator
+//
+//  Created by Kunal Rajesh Pedhambkar on 7/11/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Work Hours Calculator/Work_Hours_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Work_Hours_CalculatorApp.swift
+//  Work Hours Calculator
+//
+//  Created by Kunal Rajesh Pedhambkar on 7/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct Work_Hours_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
